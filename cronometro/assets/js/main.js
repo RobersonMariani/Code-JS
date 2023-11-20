8f91909fa443ecd0fb5f755f088f2d268d19320e
unknown <titi.mariani.rm@gmail.com>
2023-11-19 23:01:02 -0300

    criação cronômetro

--- cronometro/assets/js/main.js
+++ cronometro/assets/js/main.js
@@ -1,19 +1,8 @@
 const timer = document.querySelector('.timer');
 const milisec = document.querySelector('.milisec');
-const btnStart = document.querySelector('.start');
-const btnPause = document.querySelector('.pause');
-const btnReset = document.querySelector('.reset');
 let interval;
 let startTime;
 let elapsedTime = 0; // variável para armazenar o tempo decorrido
-
-function startTimer() {
-    if (!interval) { // Inicia somente se o cronômetro não estiver rodando
-        startTime = new Date() - elapsedTime; // Ajusta o startTime com base no tempo decorrido
-        timer.style.color = 'black';
-        interval = setInterval(updateTimer, 1);
-    }
-}
 
 function updateTimer() {
     const now = new Date();
@@ -25,6 +14,14 @@ function updateTimer() {
     const milisec = Math.floor((elapsedTime % 1000) / 10).toString().padStart(2, '0');
 
     timer.innerHTML = `${hours}:${minutes}:${seconds}.<span class="milisec">${milisec}</span>`;
+}
+
+function startTimer() {
+    if (!interval) { // Inicia somente se o cronômetro não estiver rodando
+        startTime = new Date() - elapsedTime; // Ajusta o startTime com base no tempo decorrido
+        timer.style.color = 'black';
+        interval = setInterval(updateTimer, 1);
+    }
 }
 
 function pauseTimer() {
@@ -43,7 +40,9 @@ function resetTimer() {
     timer.innerHTML = '00:00:00.<span class="milisec">00</span>';
     milisec.style.fontSize = '0.8em';
 }
-
-btnStart.addEventListener('click', startTimer);
-btnPause.addEventListener('click', pauseTimer);
-btnReset.addEventListener('click', resetTimer);
+document.addEventListener('click', function (e) {
+    let el = e.target;
+    if (el.classList.contains('start')) { startTimer(); }
+    if (el.classList.contains('pause')) { pauseTimer(); }
+    if (el.classList.contains('reset')) { resetTimer(); }
+});
